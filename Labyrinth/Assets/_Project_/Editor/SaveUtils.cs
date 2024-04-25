d65ced863779f2a0973b84ffe7bfdaa8b233b73d
D3RRIXX <jenyazagevskii@gmail.com>
2024-04-25 10:42:21 +0300

    Added some save utils

---
+++ Labyrinth/Assets/_Project_/Editor/SaveUtils.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Labyrinth.Infrastructure.SaveSystem;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Editor
+{
+	public static class SaveUtils
+	{
+		[MenuItem("Tools/Open Save Folder")]
+		public static void OpenPersistentDataPathInExplorer()
+		{
+			string path = Application.persistentDataPath;
+
+			if (!Directory.Exists(path))
+			{
+				Debug.LogWarning($"Persistent data path does not exist: {path}");
+				return;
+			}
+
+			Process.Start("explorer.exe", path);
+		}
+
+		[MenuItem("Tools/Delete Game Save")]
+		private static void DeleteSave()
+		{
+			if (!File.Exists(SaveManager.SAVE_PATH))
+				return;
+			
+			File.Delete(SaveManager.SAVE_PATH);
+		}
+	}
+}
